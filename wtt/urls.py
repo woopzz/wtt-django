41540d1138733792e1e09c138943593930b52c85
woopzz <bkovtunovych@gmail.com>
2025-03-22 11:55:41 +0000

    Add DRF and token based authentication
    102010907

--- wtt/urls.py
+++ wtt/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.authtoken import views as authtoken_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', authtoken_views.obtain_auth_token),
 ]
