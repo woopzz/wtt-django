016e85b49c0e8181ead26e08fbcd099a9617e234
woopzz <bkovtunovych@gmail.com>
2025-05-14 13:14:42 +0000

    Limit the max length of a note.
    103212490

--- wtt/work_sessions/serializers.py
+++ wtt/work_sessions/serializers.py
@@ -4,6 +4,8 @@ from .models import WorkSession
 
 
 class WorkSessionSerializer(serializers.ModelSerializer):
+    note = serializers.CharField(allow_blank=True, required=False, max_length=1000)
+
     class Meta:
         model = WorkSession
         fields = ['id', 'started_at', 'ended_at', 'duration', 'note']
