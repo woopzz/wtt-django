298079a1e22203053890a2aaada8e771e85e4302
woopzz <bkovtunovych@gmail.com>
2025-05-16 12:04:17 +0000

    Use ViewSet.
    103212490

--- wtt/work_sessions/urls.py
+++ wtt/work_sessions/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import work_session_list, work_session, work_session_end
+from .views import WorkSessionViewSet
 
 urlpatterns = [
-    path('', work_session_list, name='work-session-list'),
-    path('<uuid:pk>/', work_session, name='work-session'),
-    path('<uuid:pk>/end/', work_session_end, name='work-session-end'),
+    path('', WorkSessionViewSet.as_view({'get': 'list', 'post': 'create'}), name='work-session-list'),
+    path('<uuid:pk>/', WorkSessionViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}), name='work-session'),
+    path('<uuid:pk>/end/', WorkSessionViewSet.as_view({'post': 'end'}), name='work-session-end'),
 ]
--- wtt/work_sessions/views.py
+++ wtt/work_sessions/views.py
@@ -1,28 +1,29 @@
 from django.db import transaction
 from django.core.exceptions import ValidationError
 
-from rest_framework import permissions, authentication
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework import permissions, authentication, mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework.status import (
-    HTTP_201_CREATED, HTTP_204_NO_CONTENT,
-    HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND,
-)
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from .models import WorkSession
 from .serializers import WorkSessionSerializer
 
-@api_view(['GET', 'POST'])
-@permission_classes([permissions.IsAuthenticated])
-@authentication_classes([authentication.TokenAuthentication])
-def work_session_list(request):
-    if request.method == 'GET':
-        ws = WorkSession.objects.all()
-        serializer = WorkSessionSerializer(ws, many=True)
-        return Response(serializer.data)
 
-    if request.method == 'POST':
+class WorkSessionViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    queryset = WorkSession.objects.all()
+    serializer_class = WorkSessionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def create(self, request):
         serializer = WorkSessionSerializer(data={})
         if serializer.is_valid():
             serializer.save()
@@ -30,20 +31,9 @@ def work_session_list(request):
 
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PATCH', 'DELETE'])
-@permission_classes([permissions.IsAuthenticated])
-@authentication_classes([authentication.TokenAuthentication])
-def work_session(request, pk):
-    try:
-        ws = WorkSession.objects.get(pk=pk)
-    except WorkSession.DoesNotExist:
-        return Response(status=HTTP_404_NOT_FOUND)
+    def partial_update(self, request, pk):
+        ws = self.get_object()
 
-    if request.method == 'GET':
-        serializer = WorkSessionSerializer(ws)
-        return Response(serializer.data)
-
-    if request.method == 'PATCH':
         data = JSONParser().parse(request)
         data = {'note': data.get('note')} if isinstance(data, dict) else {}
 
@@ -54,32 +44,23 @@ def work_session(request, pk):
 
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-    if request.method == 'DELETE':
-        ws.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+    @action(detail=True)
+    def end(self, request, pk):
+        ws = self.get_object()
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-@authentication_classes([authentication.TokenAuthentication])
-def work_session_end(request, pk):
-    try:
-        ws = WorkSession.objects.get(pk=pk)
-    except WorkSession.DoesNotExist:
-        return Response(status=HTTP_404_NOT_FOUND)
+        with transaction.atomic():
+            try:
+                ws.end()
+            except ValidationError as exc:
+                return Response({'detail': str(exc)}, status=HTTP_400_BAD_REQUEST)
 
-    with transaction.atomic():
-        try:
-            ws.end()
-        except ValidationError as exc:
-            return Response({'detail': str(exc)}, status=HTTP_400_BAD_REQUEST)
+            data = request.data if request.body else {}
+            serializer = WorkSessionSerializer(ws, data=data)
+            serializer.is_valid(raise_exception=True)
 
-        data = request.data if request.body else {}
-        serializer = WorkSessionSerializer(ws, data=data)
-        serializer.is_valid(raise_exception=True)
+            new_note = serializer.validated_data.get('note')
+            if new_note:
+                ws.note = new_note
+                ws.save()
 
-        new_note = serializer.validated_data.get('note')
-        if new_note:
-            ws.note = new_note
-            ws.save()
-
-        return Response(serializer.data)
+            return Response(serializer.data)
